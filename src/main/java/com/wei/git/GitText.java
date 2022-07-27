4bcf5755f230de8fe738a66429062c9a56cb31c1
chenhewei <1990945244@qq.com>
2022-07-27 22:03:19 +0800

    first commit

---
+++ src/main/java/com/wei/git/GitText.java
@@ -0,0 +1,8 @@
+package com.wei.git;
+
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!!");
+        System.out.println("hello Git2!!!");
+    }
+}
